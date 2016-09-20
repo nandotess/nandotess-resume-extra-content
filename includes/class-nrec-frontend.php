6a7eede18a192a84be3ee5da4f4841c2923cee1b
Fernando Tessmann <nandotess85@gmail.com>
2016-09-20 10:50:12 -0300

    initial commits

--- includes/class-nrec-frontend.php
+++ includes/class-nrec-frontend.php
@@ -28,7 +28,6 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 			add_action( 'nr_homepage', array( $this, 'homepage_section_skills' ), 30 );
 			add_action( 'nr_homepage', array( $this, 'homepage_section_works' ), 40 );
 			add_action( 'nr_homepage', array( $this, 'homepage_section_portfolio' ), 50 );
-			add_action( 'nr_homepage', array( $this, 'homepage_section_education' ), 60 );
 			add_action( 'nr_homepage', array( $this, 'homepage_section_contact' ), 70 );
 		}
 
@@ -52,9 +51,13 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 		 */
 		public function homepage_section_about_buttons() {
 			?>
-			<div class="section-buttons">
-				<a href="#" class="btn btn-default btn-lg">Download CV <span class="fa fa-download" aria-hidden="true"></span></a>
-				<a href="#" class="btn btn-primary btn-lg">Contact me <span class="fa fa-send" aria-hidden="true"></span></a>
+			<div class="row row-buttons">
+				<div class="col-xs-12 col-sm-6 col-button-cv">
+					<a href="#" class="btn btn-default btn-lg">Download CV <span class="fa fa-download" aria-hidden="true"></span></a>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-button-contact">
+					<a href="#" class="btn btn-primary btn-lg">Contact me <span class="fa fa-send" aria-hidden="true"></span></a>
+				</div>
 			</div>
 			<?php
 		}
@@ -70,9 +73,13 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 			?>
 			<section id="skills" class="section skills">
 				<div class="container">
-					<div class="row">
+					<div class="row row-title">
 						<div class="col-md-12">
-							<h2 class="title">Skills</h2>
+							<h2 class="title"><span class="fa fa-cogs" aria-hidden="true"></span> Skills</h2>
+						</div>
+					</div>
+					<div class="row row-content">
+						<div class="col-md-12">
 							<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue eleifend, placerat nulla eget, sollicitudin nisl. Mauris auctor porttitor est, ut tempus risus sodales id. Morbi suscipit in arcu id tempor. Nam eleifend lobortis elit, vitae pellentesque neque ultricies sed. In sed magna interdum, dapibus neque non, pharetra lorem. Nullam venenatis libero nec fermentum convallis. Aenean posuere tortor ultricies risus feugiat, id placerat risus pharetra. Quisque porta ipsum id sodales rhoncus.</p>
 						</div>
 					</div>
@@ -92,9 +99,13 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 			?>
 			<section id="works" class="section works">
 				<div class="container">
-					<div class="row">
+					<div class="row row-title">
 						<div class="col-md-12">
-							<h2 class="title">Works</h2>
+							<h2 class="title"><span class="fa fa-suitcase" aria-hidden="true"></span> Works</h2>
+						</div>
+					</div>
+					<div class="row row-content">
+						<div class="col-md-12">
 							<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue eleifend, placerat nulla eget, sollicitudin nisl. Mauris auctor porttitor est, ut tempus risus sodales id. Morbi suscipit in arcu id tempor. Nam eleifend lobortis elit, vitae pellentesque neque ultricies sed. In sed magna interdum, dapibus neque non, pharetra lorem. Nullam venenatis libero nec fermentum convallis. Aenean posuere tortor ultricies risus feugiat, id placerat risus pharetra. Quisque porta ipsum id sodales rhoncus.</p>
 						</div>
 					</div>
@@ -114,31 +125,13 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 			?>
 			<section id="portfolio" class="section portfolio">
 				<div class="container">
-					<div class="row">
+					<div class="row row-title">
 						<div class="col-md-12">
-							<h2 class="title">Portfolio</h2>
-							<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue eleifend, placerat nulla eget, sollicitudin nisl. Mauris auctor porttitor est, ut tempus risus sodales id. Morbi suscipit in arcu id tempor. Nam eleifend lobortis elit, vitae pellentesque neque ultricies sed. In sed magna interdum, dapibus neque non, pharetra lorem. Nullam venenatis libero nec fermentum convallis. Aenean posuere tortor ultricies risus feugiat, id placerat risus pharetra. Quisque porta ipsum id sodales rhoncus.</p>
+							<h2 class="title"><span class="fa fa-th-list" aria-hidden="true"></span> Portfolio</h2>
 						</div>
 					</div>
-				</div>
-			</section>
-			<?php
-		}
-
-		/**
-		 * Homepage section education
-		 *
-		 * @access  public
-		 * @since   1.0.0
-		 * @return  void
-		 */
-		public function homepage_section_education() {
-			?>
-			<section id="education" class="section education">
-				<div class="container">
-					<div class="row">
+					<div class="row row-content">
 						<div class="col-md-12">
-							<h2 class="title">Education</h2>
 							<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue eleifend, placerat nulla eget, sollicitudin nisl. Mauris auctor porttitor est, ut tempus risus sodales id. Morbi suscipit in arcu id tempor. Nam eleifend lobortis elit, vitae pellentesque neque ultricies sed. In sed magna interdum, dapibus neque non, pharetra lorem. Nullam venenatis libero nec fermentum convallis. Aenean posuere tortor ultricies risus feugiat, id placerat risus pharetra. Quisque porta ipsum id sodales rhoncus.</p>
 						</div>
 					</div>
@@ -158,9 +151,13 @@ if ( ! class_exists( 'NREC_Frontend' ) ) :
 			?>
 			<section id="contact" class="section contact">
 				<div class="container">
-					<div class="row">
+					<div class="row row-title">
 						<div class="col-md-12">
-							<h2 class="title">Contact</h2>
+							<h2 class="title"><span class="fa fa-send" aria-hidden="true"></span> Contact</h2>
+						</div>
+					</div>
+					<div class="row row-content">
+						<div class="col-md-12">
 							<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue eleifend, placerat nulla eget, sollicitudin nisl. Mauris auctor porttitor est, ut tempus risus sodales id. Morbi suscipit in arcu id tempor. Nam eleifend lobortis elit, vitae pellentesque neque ultricies sed. In sed magna interdum, dapibus neque non, pharetra lorem. Nullam venenatis libero nec fermentum convallis. Aenean posuere tortor ultricies risus feugiat, id placerat risus pharetra. Quisque porta ipsum id sodales rhoncus.</p>
 						</div>
 					</div>
